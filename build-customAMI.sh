440a64ba52efbd06fe694d0c1670896726832f35
thiyagsruler <94433430+thiyagsruler@users.noreply.github.com>
2021-11-16 14:35:27 +0530

    Update build-customAMI.sh

--- build-customAMI.sh
+++ build-customAMI.sh
@@ -7,5 +7,5 @@ sudo apt-get update -y
 sudo apt-get install tomcat9-admin tomcat9-common -y
 sudo apt-get install tomcat9 -y
 cd /var/lib/tomcat9/webapps/
-sudo wget https://app-ion-dev-bucket-us-east-2.s3.us-east-2.amazonaws.com/ion.war
+sudo wget https://app-ion-dev-buck-us-east-2.s3.us-east-2.amazonaws.com/ion.war
 sudo systemctl start tomcat9
